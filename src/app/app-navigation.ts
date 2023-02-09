23e66ebc0385b6c0b8d39b0731b0a4dabeb438c1
CADViewer <61973235+CADViewer@users.noreply.github.com>
2023-02-09 10:52:01 +0100

    CADViewer v8.23.2 first commit"

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,31 @@
+export const navigation = [
+  {
+    text: 'Home',
+    path: '/home',
+    icon: 'home'
+  },
+  {
+    text: 'CADViewer Sample',
+    icon: 'favorites',
+    items: [
+      {
+        text: 'CADViewer',
+        path: '/cadviewer'
+      }
+    ]
+  },
+  {
+    text: 'Examples',
+    icon: 'folder',
+    items: [
+      {
+        text: 'Profile',
+        path: '/profile'
+      },
+      {
+        text: 'Tasks',
+        path: '/tasks'
+      }
+    ]
+  }
+];
